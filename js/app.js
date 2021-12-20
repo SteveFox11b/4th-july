65a68c9d66836a3e1cd5717b2864beff95e7e41f
Stephen Horne <stevefoxhorne11b@hotmail.com>
2021-12-20 13:24:02 -0800

    javascript 4thjuly

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+    let result = prompt("Are you ready to party?");
+let message = "";
+
+if (result === "yes") {
+  message = "Then your invited!";
+}
+else {
+  message = "Sorry please don't come..."
+}
+
+document.write(message);
+
